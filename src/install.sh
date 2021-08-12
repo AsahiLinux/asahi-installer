9e1ed3e68cee9572de3170b42249afe43ca879e5
Hector Martin <marcan@marcan.st>
2021-08-13 02:30:27 +0900

    src/install.sh: Remove dead IPSW variable stuff
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- src/install.sh
+++ src/install.sh
@@ -6,11 +6,6 @@ set -e
 export LC_ALL=C
 export LANG=C
 
-if [ -z "$IPSW" ]; then
-    #IPSW=https://updates.cdn-apple.com/2021SummerSeed/fullrestores/071-63742/90636AD6-5E0A-4474-B652-A6A5AF4995E2/UniversalMac_12.0_21A5284e_Restore.ipsw
-    export IPSW=http://raider.lan:5000/UniversalMac_12.0_21A5284e_Restore.ipsw
-fi
-
 export DYLD_FALLBACK_LIBRARY_PATH=$PWD/Frameworks/Python.framework/Versions/Current/lib
 export DYLD_FALLBACK_FRAMEWORK_PATH=$PWD/Frameworks
 python=Frameworks/Python.framework/Versions/3.9/bin/python3.9
