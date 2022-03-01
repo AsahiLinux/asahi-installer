f11003686bb717f1dcd80ce353e8024f968f0661
Hector Martin <marcan@marcan.st>
2022-03-01 22:57:42 +0900

    tools: Add some misc shell scripts
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ tools/cleanbp.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+cat > /tmp/uuids.txt <<EOF
+3D3287DE-280D-4619-AAAB-D97469CA9C71
+C8858560-55AC-400F-BBB9-C9220A8DAC0D
+EOF
+
+diskutil apfs listVolumeGroups >> /tmp/uuids.txt
+
+cd /System/Volumes/iSCPreboot
+
+for i in ????????-????-????-????-????????????; do
+    if grep -q "$i" /tmp/uuids.txt; then
+        echo "KEEP $i"
+    else
+        echo "RM $i"
+        rm -rf "$i"
+    fi
+done
---
+++ tools/wipe-linux.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+diskutil list | grep Apple_APFS | grep '2\.5 GB' | sed 's/.* //g' | while read i; do
+    diskutil apfs deleteContainer "$i"
+done
+diskutil list /dev/disk0 | grep -Ei 'asahi|linux|EFI' | sed 's/.* //g' | while read i; do
+    diskutil eraseVolume free free "$i"
+done
+
+cat > /tmp/uuids.txt <<EOF
+3D3287DE-280D-4619-AAAB-D97469CA9C71
+C8858560-55AC-400F-BBB9-C9220A8DAC0D
+EOF
+
+diskutil apfs listVolumeGroups >> /tmp/uuids.txt
+
+cd /System/Volumes/iSCPreboot
+
+for i in ????????-????-????-????-????????????; do
+    if grep -q "$i" /tmp/uuids.txt; then
+        echo "KEEP $i"
+    else
+        echo "RM $i"
+        rm -rf "$i"
+    fi
+done
