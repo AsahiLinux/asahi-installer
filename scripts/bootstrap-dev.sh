1808aa508e090db433477de6d54940e271f773f1
Hector Martin <marcan@marcan.st>
2022-03-17 20:34:25 +0900

    scripts: Add new bootstrap scripts
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ scripts/bootstrap-dev.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+# SPDX-License-Identifier: MIT
+
+set -e
+
+export LANG=C
+export LC_ALL=C
+
+export VERSION_FLAG=https://de.mirror.asahilinux.org/installer-dev/latest
+export INSTALLER_BASE=https://cdn.asahilinux.org/installer-dev
+export INSTALLER_DATA=https://github.com/AsahiLinux/asahi-installer/raw/main/data/installer_data.json
+export REPO_BASE=https://cdn.asahilinux.org
+
+#TMP="$(mktemp -d)"
+TMP=/tmp/asahi-install
+
+echo
+echo "Bootstrapping installer:"
+
+mkdir -p "$TMP"
+cd "$TMP"
+
+echo "  Checking version..."
+
+PKG_VER="$(curl -s -L "$VERSION_FLAG")"
+echo "  Version: $PKG_VER"
+
+PKG="installer-$PKG_VER.tar.gz"
+
+echo "  Downloading..."
+
+curl -s -L -o "$PKG" "$INSTALLER_BASE/$PKG"
+curl -s -L -O "$INSTALLER_DATA"
+
+echo "  Extracting..."
+
+tar xf "$PKG"
+
+echo "  Initializing..."
+echo
+
+if [ "$USER" != "root" ]; then
+    echo "The installer needs to run as root."
+    echo "Please enter your sudo password if prompted."
+    exec sudo -E ./install.sh "$@"
+else
+    exec ./install.sh "$@"
+fi
