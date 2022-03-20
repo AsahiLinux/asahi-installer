8cd5d3b222edd249343de36ddde39fa15e6e2e49
Hector Martin <marcan@marcan.st>
2022-03-20 23:01:50 +0900

    install.sh: Put standard PATH directories in front
    Apparently some people are getting xattr shipped with Python.framework
    in front of the system xattr, and that breaks the stub. Let's make sure
    nothing overrides standard system binaries.
    
    Fixes: #30
    Signed-off-by: Hector Martin <marcan@marcan.st>

--- src/install.sh
+++ src/install.sh
@@ -10,7 +10,7 @@ export DYLD_LIBRARY_PATH=$PWD/Frameworks/Python.framework/Versions/Current/lib
 export DYLD_FRAMEWORK_PATH=$PWD/Frameworks
 python=Frameworks/Python.framework/Versions/3.9/bin/python3.9
 export SSL_CERT_FILE=$PWD/Frameworks/Python.framework/Versions/Current/etc/openssl/cert.pem
-export PATH="$PWD/bin:$PATH"
+export PATH="$PWD/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:$PATH"
 
 arch=
 
