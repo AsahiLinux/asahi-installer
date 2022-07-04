cf962445acd49a920fa3b0db4ef998362f063e15
Hector Martin <marcan@marcan.st>
2022-07-04 22:22:51 +0900

    test.sh: Add a script to build & run the installer locally "in-place" from macOS
    This assumes you have the necessary dependencies installed (rust, llvm, python3, etc.)
    
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+
+cd "$(dirname "$0")"
+base="$PWD"
+
+if [ -e $HOME/.cargo/env ] ; then
+    source $HOME/.cargo/env
+fi
+
+export INSTALLER_BASE=https://cdn.asahilinux.org/installer-dev
+export REPO_BASE=https://cdn.asahilinux.org
+
+make -C "m1n1" RELEASE=1 CHAINLOADING=1 -j4
+
+sudo rm -rf /tmp/asahi-install
+mkdir -p /tmp/asahi-install
+
+git describe --tags --always --dirty > /tmp/asahi-install/version.tag
+
+cd /tmp/asahi-install
+ln -sf "$base/src"/* .
+mkdir -p boot
+ln -sf "$base/m1n1/build/m1n1.bin" boot/m1n1.bin
+ln -sf "$base/artwork/logos/icns/AsahiLinux_logomark.icns" logo.icns
+ln -sf "$base/data/installer_data.json" installer_data.json
+
+sudo -E python3 $base/src/main.py
