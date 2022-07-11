66e50010ec6873bbdd3e4b0adf348b47935183be
Hector Martin <marcan@marcan.st>
2022-07-11 22:15:33 +0900

    asahi_firmware.update: Add updater module & setup.py
    Signed-off-by: Hector Martin <marcan@marcan.st>

---
+++ asahi_firmware/update.py
@@ -0,0 +1,40 @@
+# SPDX-License-Identifier: MIT
+import pathlib, tempfile, subprocess
+
+from .core import FWPackage
+from .wifi import WiFiFWCollection
+from .bluetooth import BluetoothFWCollection
+
+def update_firmware(source, dest, manifest):
+    raw_fw = source.joinpath("all_firmware.tar.gz")
+    if not raw_fw.exists():
+        print(f"Could not find {raw_fw}")
+    
+    pkg = FWPackage(dest)
+
+    with tempfile.TemporaryDirectory() as tmpdir:
+        tmpdir = pathlib.Path(tmpdir)
+        subprocess.run(["tar", "xf", str(raw_fw.resolve())], cwd=tmpdir, check=True)
+        col = WiFiFWCollection(str(tmpdir.joinpath("firmware", "wifi")))
+        pkg.add_files(sorted(col.files()))
+        col = BluetoothFWCollection(str(tmpdir.joinpath("firmware", "bluetooth")))
+        pkg.add_files(sorted(col.files()))
+
+    pkg.close()
+
+    pkg.save_manifest(manifest)
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description='Update vendor firmware tarball')
+    parser.add_argument('source', metavar='DIR', type=pathlib.Path,
+                        help='path containing raw firmware')
+    parser.add_argument('dest', metavar='FILE', type=pathlib.Path,
+                        help='output vendor firmware tarball')
+    parser.add_argument('manifest', metavar='FILE', type=pathlib.Path,
+                        help='output vendor firmware manifest')
+    
+    args = parser.parse_args()
+
+    update_firmware(args.source, args.dest, args.manifest)
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='asahi_firmware',
+      version='0.1',
+      description='Asahi Linux firmware tools',
+      author='Hector Martin',
+      author_email='marcan@marcan.st',
+      url='https://github.com/AsahiLinux/asahi-installer/',
+      packages=['asahi_firmware'],
+     )
