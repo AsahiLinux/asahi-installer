dca957a91e5aa07bce4812f9e0e7acdaf99f7cdd
Asahi Lina <lina@asahilina.net>
2023-08-02 20:24:21 +0900

    asahi_firmware.img4: Autodetect compression
    Signed-off-by: Asahi Lina <lina@asahilina.net>

--- asahi_firmware/kernel.py
+++ asahi_firmware/kernel.py
@@ -26,7 +26,7 @@ class KernelFWCollection(object):
 
         with open(kern_path, "rb") as fd:
             im4p = fd.read()
-        name, kernel = img4p_extract_compressed(im4p)
+        name, kernel = img4p_extract(im4p)
 
         for fwf in extract_asmedia(kernel):
             self.fwfiles.append((fwf.name, fwf))
